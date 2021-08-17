16c471ffa0d9ee4827375416ee83fbedc2d5921e
i1-kmv <i1-kmv@mail.ru>
2021-08-17 21:42:11 +0200

    learnVue

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const App = {
-
+    data:() => ({
+        title: 'Hello Vue',
+        myHtml: '<h3>Vue3 app<h3>',
+        person: {
+            firstName: 'Ivan',
+            lastName: 'Yarovoy',
+            age: 30
+        },
+        items: [1,2,3,4,5,6]
+    }),
+    methods: {
+        stopPropagation(e) {
+            e.stopPropagation()
+        }
+    },
+    computed: {
+        evenItems() {
+            return this.items.filter(i => i % 2 === 0  )
+        }
+    }
 }
 
 Vue.createApp(App).mount('#app')
